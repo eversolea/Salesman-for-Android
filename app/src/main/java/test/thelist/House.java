11728c7bdb5d7cf10a207f8f988a7ab364fd1fd5
eversolea <eve12004@byui.edu>
2015-12-11 15:55:50 -0700

    adding matts code

---
+++ app/src/main/java/test/thelist/House.java
@@ -0,0 +1,117 @@
+package test.thelist;//package com.example.john.door_smart;
+
+import java.io.*;
+import java.util.Date;
+//import com.google.android.gms.maps.model.LatLng;
+
+public class House {
+	 // private double xCoord;
+	 // private double yCoord;
+	 //private LatLng coord;
+	 private String name;
+	 private int response;
+	 private String comment;
+	 private Address address;
+	 private Date date;
+
+     // public House(double x, double y, int myResponse, String myComment, String myAddress, Date myDate)
+     // public House(LatLng myCoord, int myResponse, String myComment, String myAddress, Date myDate)
+	 public House(String myName, int myResponse, String myComment, Address myAddress, Date myDate) {
+	 	// xCoord = x;
+	 	// yCoord = y;
+	 	//coord = myCoord;
+	 	name = myName;
+	 	response = myResponse;
+	 	comment = myComment;
+	 	address = myAddress;
+	 	date = myDate;
+	 }
+
+	 // public double getXCoord() {
+	 // 	return xCoord;
+	 // }
+
+	 // public void setXCoord(float x) {
+	 // 	xCoord = x;
+	 // }
+
+	 // public double getYCoord() {
+	 // 	return yCoord;
+	 // }
+
+	 // public void setYCoord(float x) {
+	 // 	yCoord = x;
+	 // }
+
+	 // public LatLng getCoord() {
+	 // 	return coord;
+	 // }
+
+	 // public void setCoord(LatLng x) {
+	 // 	coord = x;
+	 // }
+
+	 public String getName() {
+	 	return name;
+	 }
+
+	 public void setName(String x) {
+	 	name = x;
+	 }
+
+	 public int getResponse() {
+	 	return response;
+	 }
+
+	 public void setResponse(int x) {
+	 	response = x;
+	 }
+
+	 public String getComment() {
+	 	return comment;
+	 }
+
+	 public void setComment(String x) {
+	 	comment = x;
+	 }
+
+	 public Address getAddress() {
+	 	return address;
+	 }
+
+	 public void setAddress(Address x) {
+	 	address = x;
+	 }
+
+	 public Date getDate() {
+	 	return date;
+	 }
+
+	 public void setDate(Date x) {
+	 	date = x;
+	 }
+
+	 public void display() {
+	 	// System.out.println("X Coordinate: " + xCoord);
+	 	// System.out.println("Y Coordinate: " + yCoord);
+	 	//System.out.println("Coordinate: " + coord.toString());
+
+	 	System.out.println("Name: " + name);
+
+	 	switch (response) {
+	 		case 1:
+	 			System.out.println("Response: Green");
+	 			break;
+	 		case 2:
+	 			System.out.println("Response: Yellow");
+	 			break;
+	 		case 3:
+	 			System.out.println("Response: Red");
+	 			break;
+	 	}
+
+	 	System.out.println("Address: " + address.getHouseNumber() + " " + address.getStreetName());
+	 	System.out.println("Date added: " + date);
+	 	System.out.println("Comment: " + comment);
+	 }
+}
---
+++ app/src/main/java/test/thelist/HouseList.java
@@ -0,0 +1,147 @@
+package test.thelist;//package com.example.john.door_smart;
+
+import java.io.*;
+import java.util.*;
+
+import test.thelist.House;
+
+public class HouseList {
+	private ArrayList<House> houses;
+
+	public HouseList() {
+		houses = new ArrayList<House>();
+	}
+
+	public HouseList(HouseList h) {
+		houses = h.houses;
+	}
+	
+	public void addHouse(House h)
+	{
+		houses.add(h);
+	}
+
+	public void displayHouses() {
+		for (int i = 0; i < houses.size(); i++) {
+			houses.get(i).display();
+			System.out.println();
+		}
+	}
+
+	public HouseList sortName()
+	{
+		HouseList h = new HouseList(this);
+
+		Collections.sort(h.houses, new nameComparator());
+		return h;
+	}
+	
+	public HouseList sortResponses()
+	{
+		HouseList h = new HouseList(this);
+
+		Collections.sort(h.houses, new responseComparator());
+		return h;
+	}
+
+	public HouseList sortDates()
+	{
+		HouseList h = new HouseList(this);
+
+		Collections.sort(h.houses, new dateComparator());
+		return h;
+	}
+
+	public HouseList sortAddress()
+	{
+		HouseList h = new HouseList(this);
+
+		Collections.sort(h.houses, new addressComparator());
+		return h;
+	}
+
+	public HouseList filterName(String myName) {
+		HouseList h = new HouseList();
+
+		for (int i = 0; i < houses.size(); i++) {
+			if (houses.get(i).getName().toLowerCase().contains(myName.toLowerCase())) {
+				h.addHouse(houses.get(i));
+			}
+		}
+
+		return h;
+	}
+
+	public HouseList filterResponse(int myResponse) {
+		HouseList h = new HouseList();
+
+		for (int i = 0; i < houses.size(); i++) {
+			if (houses.get(i).getResponse() == myResponse) {
+				h.addHouse(houses.get(i));
+			}
+		}
+
+		return h;
+	}
+
+	public HouseList filterDate(Date myDate) {
+		HouseList h = new HouseList();
+
+		for (int i = 0; i < houses.size(); i++) {
+			if (houses.get(i).getDate().equals(myDate)) {
+				h.addHouse(houses.get(i));
+			}
+		}
+
+		return h;
+	}
+
+	public HouseList filterComment(String myComment) {
+		HouseList h = new HouseList();
+
+		for (int i = 0; i < houses.size(); i++) {
+			if (houses.get(i).getComment().toLowerCase().contains(myComment.toLowerCase())) {
+				h.addHouse(houses.get(i));
+			}
+		}
+
+		return h;
+	}
+
+	//This is a nested class!
+	public class nameComparator implements Comparator<House> {
+    @Override
+		public int compare(House h1, House h2) {
+			return h1.getName().compareTo(h2.getName());
+		}
+	}
+	
+	//This is a nested class!
+	public class responseComparator implements Comparator<House> {
+    @Override
+		public int compare(House h1, House h2) {
+			return Integer.compare(h1.getResponse(), h2.getResponse());
+		}
+	}
+
+	//This is a nested class!
+	public class dateComparator implements Comparator<House> {
+    @Override
+		public int compare(House h1, House h2) {
+			return h1.getDate().compareTo(h2.getDate());
+		}
+	}	
+
+	//This is a nested class!
+	public class addressComparator implements Comparator<House> {
+    @Override
+		public int compare(House h1, House h2) {
+			if (h1.getAddress().getStreetName() != h2.getAddress().getStreetName()) {
+				return h1.getAddress().getStreetName().compareTo(h2.getAddress().getStreetName());
+			}
+			else {
+				return Integer.compare(h1.getAddress().getHouseNumber(), h2.getAddress().getHouseNumber());
+			}
+		}
+	}
+}
